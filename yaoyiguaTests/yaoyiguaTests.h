ad861b4130c42f79e5f548cd9d9714258f2d80fc
suxianbaozi <suxianbaozi@suxianbaozimatoMacBook.local>
2012-06-04 22:50:32 +0800

    Initial Commit

---
+++ yaoyiguaTests/yaoyiguaTests.h
@@ -0,0 +1,13 @@
+//
+//  yaoyiguaTests.h
+//  yaoyiguaTests
+//
+//  Created by suxianbaozi on 12-6-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface yaoyiguaTests : SenTestCase
+
+@end
---
+++ yaoyiguaTests/yaoyiguaTests.m
@@ -0,0 +1,32 @@
+//
+//  yaoyiguaTests.m
+//  yaoyiguaTests
+//
+//  Created by suxianbaozi on 12-6-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "yaoyiguaTests.h"
+
+@implementation yaoyiguaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in yaoyiguaTests");
+}
+
+@end
